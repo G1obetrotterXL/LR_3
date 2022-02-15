5d482911433edb124beef75f8f2863d82ce2728f
TimurPuchinsky <timur.2.puchinsky@gmail.com>
2022-02-15 08:51:33 +0300

    гипотеза коллатза

--- main.py
+++ main.py
@@ -8,4 +8,9 @@ def x3_1():
     pass
 
 def collatz():
-    pass
+    while x != 1:
+        if x % 2 == 0:
+            return x2(x)
+    else:
+        return x3_1(x)
+    print('Список имеет вид:', sp)
